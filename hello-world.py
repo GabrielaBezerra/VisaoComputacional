94a1f52065d8a497aeaf5bccb558f4125bb5d382
Gabriela Bezerra <gabrieladecarvalhobezerra@gmail.com>
2018-02-28 20:16:17 -0300

    Installed pycharm. Hello-world.py is pecurring all pixels of an 10x10 image. Ready to implement algorythms.

--- hello-world.py
+++ hello-world.py
@@ -1,13 +1,49 @@
-from __future__ import print_function
-import sys
-
+# from __future__ import print_function
+import numpy as np
 import cv2 as cv
 
 if cv is None:
-  print("no cv shit")
+    print("Couldn't import opencv")
 
-img = cv.imread("../../opencv-3.4.0/samples/data/home.jpg",1)
-if img is None :
-  print("Problem reading image")
-cv.imshow("img",img)
+img1 = cv.imread("imgs/home_icon_10x10.png", 1)
+
+if img1 is None:
+    print("Problem reading image 1")
+
+img2 = cv.imread("../../opencv-3.4.0/samples/data/home.jpg", 1)
+if img2 is None:
+    print("Problem reading image 2")
+
+newImg = img1
+
+cont = 0
+
+# varre todos os pixels sem indexar com ndarray
+# for shape in img1:
+#     for pixel in shape:
+#         for value in pixel:
+#             cont += 1
+#             print value
+
+cv.imshow("antes", img1)
+
+# converte ndarray pra lista
+data1 = np.array(img1, dtype=int)
+list1 = data1.tolist()
+
+# varre todos os pixels indexando com uma lista
+for i, column in enumerate(list1):
+    for j, row in enumerate(column):
+        for p, pixel in enumerate(row):
+            cont += 1
+            if img1[i][j][p] - 200 < 0:
+                img1[i][j][p] = 0
+            else:
+                img1[i][j][p] -= 200
+
+
+print ""
+print cont
+
+cv.imshow("depois", img1)
 cv.waitKey(0)
