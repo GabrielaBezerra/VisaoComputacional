bf2756bd97814b54bcc461e238ae8abbf094b005
Gabriela Bezerra <gabrieladecarvalhobezerra@gmail.com>
2018-03-08 18:25:17 -0300

    feat: Summing images. Method: ugly conversion of nparray to list for working with matrices as if it were C :(

--- SomaImagens.py
+++ SomaImagens.py
@@ -1,4 +1,3 @@
-# from __future__ import print_function
 import numpy as np
 import cv2 as cv
 
@@ -9,10 +8,13 @@ img1 = cv.imread("imgs/home_icon_10x10.png", 1)
 if img1 is None:
     print("Problem reading image 1")
 
-img2 = cv.imread("../../opencv-3.4.0/samples/data/home.jpg", 1)
+img2 = cv.imread("imgs/home_icon2_10x10.png", 1)
 if img2 is None:
     print("Problem reading image 2")
 
+
+# TODO: Ver como atribui cópia em python
+# passando por referencia, as alterações estão sendo feitas na img1.
 newImg = img1
 
 cont = 0
@@ -24,27 +26,24 @@ cont = 0
 #             cont += 1
 #             print value
 
-cv.imshow("antes", img1)
-
+cv.imshow("antes 1", img1)
+cv.imshow("antes 2", img2)
 
 # converte ndarray pra lista
 data1 = np.array(img1, dtype=int)
 data2 = np.array(img2, dtype=int)
 list1 = data1.tolist()
+list2 = data2.tolist()
 
 # varre todos os pixels indexando com uma lista
 for i, column in enumerate(list1):
     for j, row in enumerate(column):
         for p, pixel in enumerate(row):
             cont += 1
-            if img1[i][j][p] - 200 < 0:
-                img1[i][j][p] = 0
-            else:
-                img1[i][j][p] -= 200
-
+            newImg[i][j][p] = list1[i][j][p] + list2[i][j][p]
 
 print ""
 print cont
 
-cv.imshow("depois", img1)
+cv.imshow("depois", newImg)
 cv.waitKey(0)
