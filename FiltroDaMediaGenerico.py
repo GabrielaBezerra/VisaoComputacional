d529970062250e29914f0bfab23bd80e22019d69
Gabriela Bezerra <gabrieladecarvalhobezerra@gmail.com>
2018-03-29 17:44:41 -0300

    refactor: Convolucao2D function created.

--- FiltroDaMediaGenerico.py
+++ FiltroDaMediaGenerico.py
@@ -1,56 +1,63 @@
 from numpy import *
 from cv2 import *
 
-#CONSTS
 
-IMAGE_PATH = "imgs/blox.jpg"
-SIZE_OF_MASK = 5
+def convolucao2D(mask, imgPath, img=None):
 
+  if img is None:
+    img = imread(imgPath)
 
-# AQUISICAO DA IMAGEM
+  newimg = imread(imgPath)
 
-img = imread(IMAGE_PATH)
-newImg = imread(IMAGE_PATH)
+  if mask.shape[0]%2 == 0:
+    print "\n# ERROR: MASK SHAPE IS EVEN! \n  SIZE OF MASK:", mask.shape[0], "\n  SIZE OF IMAGE:", img.shape[0]
+    return
 
+  if mask.shape[0] > img.shape[0]:
+    print "\n# ERROR: MASK IS TOO BIG! \n  SIZE OF MASK:", mask.shape[0], "\n  SIZE OF IMAGE:", img.shape[0]
+    return
 
+  center_index_mask = int(mask.shape[0] / 2)
 
-
-
-# PRE-PROCESSAMENTO
-
-mask = ones((SIZE_OF_MASK, SIZE_OF_MASK, 3))
-center_index_mask = int(mask.shape[0] / 2)
-
-if SIZE_OF_MASK > mask.shape[0]:
-    print "MASK IS TOO BIG!"
-
-for x in range(img.shape[0]):
+  for x in range(img.shape[0]):
 
     for y in range(img.shape[1]):
 
-        for p in range(3):
+      for p in range(3):
 
-            # pixel access
-            ac = 0
+        # pixel access
+        ac = 0
 
-            for v1 in range(-center_index_mask, center_index_mask + 1, 1):
+        for v1 in range(-center_index_mask, center_index_mask + 1, 1):
 
-                for v2 in range(-center_index_mask, center_index_mask + 1, 1):
+          for v2 in range(-center_index_mask, center_index_mask + 1, 1):
 
-                    if x + v1 < 0 or y + v2 < 0 or x + v1 > img.shape[0] - 1 or y + v2 > img.shape[1] - 1:
-                        ac += 1
-                        # print "outbounds ", "p:", p," [x:",x,", y:",y,"]","* (",v1+cim,",",v2+cim,")   ", ac
-                    else:
-                        ac += (mask[center_index_mask + v1][center_index_mask + v2][p] * img[x + v1][y + v2][p])
-                        # print "inbounds  ", "p:", p," [x:",x,", y:",y,"]","* (",v1+cim,",",v2+cim,")   ", ac
+            if x + v1 < 0 or y + v2 < 0 or x + v1 > img.shape[0] - 1 or y + v2 > img.shape[1] - 1:
+              ac += int(mask[v1][v2][p])
+              # print "outbounds ", "p:", p," [x:",x,", y:",y,"]","* (",v1+center_index_mask,",",v2+center_index_mask,")   ", ac
+            else:
+              ac += (mask[center_index_mask + v1][center_index_mask + v2][p] * img[x + v1][y + v2][p])
+              # print "inbounds  ", "p:", p," [x:",x,", y:",y,"]","* (",v1+center_index_mask,",",v2+center_index_mask,")   ", ac
+
+          newimg[x][y][p] = int(ac / (mask.shape[0]*mask.shape[1]))
+
+  return newimg
 
 
-            newImg[x][y][p] = int(ac / (mask.shape[0] * mask.shape[1]))
 
 
 
 
-# MOSTRA RESULTADO
+# Main
 
-imshow("result", newImg)
-waitKey(0)
+image_path = "imgs/blox.jpg"
+size_of_mask = 3
+
+img = imread(image_path)
+mask = ones((size_of_mask, size_of_mask, 3))
+
+result = convolucao2D(mask, image_path)
+
+if result is not None:
+  imshow("result", result)
+  waitKey(0)
